91d8e5d7f2edf8fe1d7a60eaf52d4841d356f212
leeminjung1 <vipminjung@gmail.com>
2022-03-22 19:07:55 +0900

    Persistence Context

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -13,30 +13,51 @@ public class JpaMain {
         EntityTransaction tx = em.getTransaction();
         tx.begin();
         try {
+/*
+            // 쓰기 지연
+            // 비영속 상태
+            Member member1 = new Member(150L, "A");
+            Member member2 = new Member(160L, "B");
 
-            // 등록
-//            Member member = new Member();
-//            member.setId(1L);
-//            member.setName("HelloA");
-//            em.persist(member);
+            // 영속 상태
+            em.persist(member1);
+            em.persist(member2);
+            System.out.println("=== 이때까지 db에 저장되지 않음 ===");
+*/
 
-            // 수정
-//            Member findMember = em.find(Member.class, 1L);
-//            findMember.setName("HelloJpa");
+/*
+            // update - dirty checking
+            Member member = em.find(Member.class, 150L);
+            member.setName("Updated");
+            System.out.println("=== em에 따로 업데이트 하지 않아도됨 ===");
+*/
 
-            // 삭제
-//            em.remove(findMember);
+/*
+            // flush 강제 호출
+            Member member200 = new Member(200L, "member200");
+            em.persist(member200);
+            System.out.println("=== start insert query");
+            em.flush();
+            System.out.println("=== end insert query");
+*/
 
-            // 수정
-            List<Member> result = em.createQuery("select m from Member as m", Member.class)
-                    .setFirstResult(5)
-                    .setMaxResults(8)
-                    .getResultList();
-            for (Member member : result) {
-                System.out.println("member.getName() = " + member.getName());
-            }
+/*
+            // 준영속
+            // 영속 상태
+            Member member = em.find(Member.class, 150L);
+            member.setName("AAAAA");
+            // 준영속 상태
+//            em.detach(member);
+            em.clear();
+            //commit시 update되지 않음
 
-            tx.commit();
+            // 다시 조회시 영속성 컨텍스트에 있지않아서 select쿼리를 다시 날림림
+           Member member1 = em.find(Member.class, 150L);
+*/
+
+
+            // commit시 DB에 query날림
+             tx.commit();
         } catch (Exception e) {
             tx.rollback();
         } finally {
