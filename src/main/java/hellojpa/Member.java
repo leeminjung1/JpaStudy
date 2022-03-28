3d56eb50a05bde294f9bb8e80ca7007715b670f9
leeminjung1 <vipminjung@gmail.com>
2022-03-28 10:20:35 +0900

    one to many

--- src/main/java/hellojpa/Member.java
+++ src/main/java/hellojpa/Member.java
@@ -13,29 +13,6 @@ public class Member {
     @Column(name = "USERNAME")
     private String username;
 
-//    @Column(name = "TEAM_ID")
-//    private Long teamId;
-
-    // 객체 지향 모델링
-    @ManyToOne
-    @JoinColumn(name = "team_id")
-    private Team team;
-
-    public Team getTeam() {
-        return team;
-    }
-
-    /* setTeam 은 관례상 사용되는 setter 느낌이므로
-    *  중요하다는 의미에서 이름 바꿔서 사용 */
-//    public void changeTeam(Team team) {
-//        this.team = team;
-//        team.getMembers().add(this);
-//    }
-
-    public void setTeam(Team team) {
-        this.team = team;
-    }
-
     public Long getId() {
         return id;
     }
@@ -51,5 +28,4 @@ public class Member {
     public void setUsername(String username) {
         this.username = username;
     }
-
 }
--- src/main/java/hellojpa/Team.java
+++ src/main/java/hellojpa/Team.java
@@ -12,13 +12,9 @@ public class Team {
     private Long id;
     private String name;
 
-    @OneToMany(mappedBy = "team")
+    @OneToMany
+    @JoinColumn(name = "TEAM_ID")   // 일대다 단방향
     private List<Member > members = new ArrayList<>();
-
-    public void addMember(Member member) {
-        member.setTeam(this);
-        members.add(member);
-    }
 
     public Long getId() {
         return id;
