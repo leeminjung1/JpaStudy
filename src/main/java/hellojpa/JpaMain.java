541ec95d6879d008dc982ce2032254aaec65fb3f
leeminjung1 <vipminjung@gmail.com>
2022-03-29 18:01:18 +0900

    cascade / orphanRemoval

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -16,40 +16,27 @@ public class JpaMain {
         EntityTransaction tx = em.getTransaction();
         tx.begin();
         try {
+            Child child1 = new Child();
+            Child child2 = new Child();
 
-            Team team1 = new Team();
-            team1.setName("teamA");
-            em.persist(team1);
+            Parent parent = new Parent();
+            parent.addChild(child1);
+            parent.addChild(child2);
 
-            Member member1 = new Member();
-            member1.setUsername("hello1");
-            member1.setTeam(team1);
-            em.persist(member1);
-
-            Team team2 = new Team();
-            team2.setName("team2");
-            em.persist(team2);
-
-            Member member2 = new Member();
-            member2.setUsername("hello2");
-            member2.setTeam(team2);
-            em.persist(member2);
-
+            em.persist(parent);     // parent는 영속성 컨테이너에서 생명주기 관리. child는 parent가 관리
+            // 엔티티 옵션 cascade = CascadeType.ALL 로 주면 parent persist 시 child 모두 자동 persist
+//            em.persist(child1);
+//            em.persist(child2);
 
             em.flush();
             em.clear();
 
-            // 멤버 조회시 멤버만 가져오기 위해
-            // 지연로딩 LAZY 사용시 team은 proxy로 조회
-            Member m = em.find(Member.class, member1.getId());
-            System.out.println("m.getTeam().getClass() = " + m.getTeam().getClass());
-            // 프록시에 없는 값 조회하는 시점에 select문 날림(프록시 초기화 요청)
-            System.out.println("m.getTeam().getName() = " + m.getTeam().getName());
+            Parent findParent = em.find(Parent.class, parent.getId());
 
-
-            // 즉시로딩 EAGER 사용시 member 조회할때 team도 join해서 같이 조회
-            // 문제는 JPQL 쿼리 사용시 N(팀 개수) +1(최초 쿼리)번 쿼리가 나감
-            List<Member> members = em.createQuery("select m from Member m", Member.class).getResultList();
+            // orphanRemoval = true: 컬렉션에서 빠진 객체는 DB에서 삭제됨
+//            findParent.getChildList().remove(0);
+            // 부모 삭제되면 자식도 삭제됨
+            em.remove(parent);
 
 
             tx.commit();
